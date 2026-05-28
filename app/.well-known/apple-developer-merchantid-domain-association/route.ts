b4897989e010b494eb905f9145b6240225f0da47
gsmt <samaritanojr006@gmail.com>
2026-05-28 20:49:24 +0200

    fix

--- app/.well-known/apple-developer-merchantid-domain-association/route.ts
+++ app/.well-known/apple-developer-merchantid-domain-association/route.ts
@@ -1,7 +1,5 @@
 import { APPLE_DOMAIN_ASSOCIATION } from "@/lib/common/constants";
 
-export const runtime = "edge";
-
 export function GET() {
     return new Response(APPLE_DOMAIN_ASSOCIATION, {
         status: 200,
