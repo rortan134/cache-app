0ddc1b1d8b11dfe767fa2428ab30a46c4df82333
gsmt <samaritanojr006@gmail.com>
2026-06-15 23:09:21 +0200

    remove telemetry

--- instrumentation.ts
+++
@@ -1,9 +0,0 @@
-import { ORPCInstrumentation } from "@orpc/otel";
-import { registerOTel } from "@vercel/otel";
-
-export function register() {
-    registerOTel({
-        instrumentations: [new ORPCInstrumentation()],
-        serviceName: "cache-app",
-    });
-}
