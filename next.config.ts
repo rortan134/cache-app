010798f5f5f052bfde3c5b2053e71f3cc404800b
gsmt <samaritanojr006@gmail.com>
2026-06-25 11:36:54 +0200

    temp

--- next.config.ts
+++ next.config.ts
@@ -161,6 +161,7 @@ const securityHeaders = [
 
 export default withWorkflow(
     withGTConfig(nextConfig, {
+        experimentalLocaleResolution: true,
         getLocalePath: "./get-locale.ts",
         getRegionPath: "./get-region.ts",
         loadTranslationsPath: "./load-translations.ts",
