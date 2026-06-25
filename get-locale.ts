da82e8a5d25c2eb3d829309ab96c5a40249bbb21
gsmt <samaritanojr006@gmail.com>
2026-06-25 21:20:56 +0200

    fix

--- get-locale.ts
+++ get-locale.ts
@@ -1,5 +1,12 @@
+import gtConfig from "./gt.config.json";
 import { locale } from "next/root-params";
 
 export default async function getLocale() {
-    return await locale();
+    const l = await locale();
+
+    if (l && gtConfig.locales.includes(l)) {
+        return l;
+    }
+
+    return gtConfig.defaultLocale;
 }
