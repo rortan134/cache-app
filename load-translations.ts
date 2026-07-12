c7700e35d97970c1dc118c231d3cd5d220cda722
gsmt <samaritanojr006@gmail.com>
2026-07-12 14:08:41 +0200

    i18n fix

--- load-translations.ts
+++ load-translations.ts
@@ -1,14 +1,4 @@
-import { readFile } from "node:fs/promises";
-import path from "node:path";
-
 export default async function loadTranslations(locale: string) {
-    const filePath = path.join(
-        /* turbopackIgnore: true */ process.cwd(),
-        "public",
-        "_gt",
-        `${locale}.json`
-    );
-
-    const translations = await readFile(filePath, "utf8");
-    return JSON.parse(translations) as Record<string, unknown>;
+    const translations = await import(`./public/_gt/${locale}.json`);
+    return translations.default;
 }
